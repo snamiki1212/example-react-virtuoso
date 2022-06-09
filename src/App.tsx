a92fa5fcf5d94d87438f6b7c13cccfd5ab314a2f
snamiki1212 <snamiki1212@gmail.com>
2022-06-09 11:35:06 -0700

    refactor;

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Stack from "@mui/material/Stack";
 import { InfiniteScroll } from "./components/InfiniteScroll/plain";
 
 function App() {
+  const { type, render, setInfiniteType } = useSwitchRender();
   return (
     <div
       style={{
@@ -22,20 +23,30 @@ function App() {
       >
         this is header
       </div>
-      <div style={{ height: "100px", background: "lightblue" }}>===start</div>
-      <Stack
-        // overflow={"scroll"} // TODO: overflow?
-        direction="row"
-        flex={1}
-      >
+
+      <div style={{ height: "100px", background: "lightblue" }}>
+        <div>this is head. type is ({type})</div>
+        <button onClick={setInfiniteType}>Infinite</button>
+      </div>
+      <Stack direction="row" flex={1}>
         <Stack style={{ background: "black", width: "50px" }}></Stack>
-        <Stack flex="1">
-          <InfiniteScroll />
-        </Stack>
+        <Stack flex="1">{render()}</Stack>
       </Stack>
       <div style={{ height: "100px", background: "lightblue" }}>===end</div>
     </div>
   );
 }
+
+const useSwitchRender = () => {
+  const [type, setType] = React.useState<"infinite">("infinite");
+  const setInfiniteType = () => setType("infinite");
+
+  const render = () => {
+    if (type === "infinite") return <InfiniteScroll />;
+    return undefined;
+  };
+
+  return { setInfiniteType, render, type };
+};
 
 export default App;
